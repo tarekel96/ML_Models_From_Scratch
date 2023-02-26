9fbec8f86c1a8ccace7c69c28c563fc3f2cb0a71
Tarek <elhaj102@mail.chapman.edu>
2023-02-25 21:42:09 -0800

    Implemented Linear Regression. Still need to implement regularization.

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -3,47 +3,59 @@
 
 import numpy as np
 
-class linear_regression():
-        def __init__(self, learning_rate) -> None:
-                # hyperparameter that determines magnitude of weight updates
-                self.learning_rate = learning_rate
+class LinearRegression():
+        def __init__(self, lr=0.01, epochs=1000) -> None:
+                # hyperparameter lr = learning rate which determines magnitude of weight updates
+                self.lr = lr
+                # number of iterations
+                self.epochs = epochs
                 # weights - a vector with N + bias components
                 self.w = None
                 # feature data
                 self.X = None
                 # output data
                 self.y = None
-                # M = number of rows (data points) in X matrix, N = number of columns (features) in X matrix
-                self.M, self.N = self.X.shape[0], self.X.shape[1]
                 # keeps track of all the costs per weight update
                 self.costs = []
         
-        def fit(self, X):
-                self.X = X
-                # initialize weights
-                self.w = np.ones((self.N + 1, 1))
+        def objective_function(self, y_residual):
+                return (1/(2 * self.M)) * np.dot(y_residual.T, y_residual)
 
-                while True:
-                        y_hat = self.predict(X)
-                        
-                        cost = self.objective_function(self.y, y_hat)
-                        self.costs.append(cost)
+        def gradient(self, y_residual):
+                return (1 / self.M) * np.dot(self.X.T, y_residual)
 
-                        gradient = self.gradient(self.y, y_hat)
-                        bias_gradient = self.bias_gradient(self.y, y_hat)
-
-                        # TODO update weights based on learning rate and gradient
-                        self.w[:-1] = self.w[:-1] - self.learning_rate * gradient
-                        self.w[-1] = self.w[-1] - self.learning_rate * bias_gradient
-
-        def objective_function(self, y, y_hat):
-                return (1/(2 * self.M)) * np.sum(np.square(y - y_hat))
-
-        def gradient(self, y, y_hat):
-                return (1 / self.M) * np.sum((y - y_hat) * self.X)
-
-        def bias_gradient(self, y, y_hat):
-                return (1 / self.M) * np.sum((y - y_hat))
+        def bias_gradient(self, y_residual):
+                return (1 / self.M) * np.sum(y_residual)
 
         def predict(self, X):
-                return np.dot(self.w[:-1], X) + self.w[-1] 
+                return np.dot(self.w[:-1], self.X.T) + self.w[-1]
+
+        def fit(self, X, y):
+                self.X = X
+                self.y = y
+                # M = number of rows (data points) in X matrix, N = number of columns (features) in X matrix
+                self.M, self.N = self.X.shape[0], 1
+                if len(self.X.shape) > 1:
+                        self.N = self.X.shape[1]
+
+                # initialize weights
+                self.w = np.ones((self.N + 1, ))
+
+                for _ in range(self.epochs):
+                        # linear prediction
+                        y_hat = self.predict(X)
+                        
+                        # residual
+                        error = y_hat - self.y
+
+                        # keep track of costs
+                        cost = self.objective_function(y_residual=error)
+                        self.costs.append(cost)
+                        
+                        # calculate gradient vectors for weight updates
+                        gradient = self.gradient(y_residual=error)
+                        bias_gradient = self.bias_gradient(y_residual=error)
+
+                        # update weights based on learning rate and gradient
+                        self.w[:-1] = self.w[:-1] - self.lr * gradient
+                        self.w[-1] = self.w[-1] - self.lr * bias_gradient
